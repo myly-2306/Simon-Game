09417236475b6cb3b30d6638dcdc4740f2614f23
My Ly <ly.thuongmy71@gmail.com>
2022-10-08 16:01:33 -0700

    evenhandler to take button id when a color is clicked

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var buttonClicked = $('.btn');
+var userClickedPattern = [];
 
 function nextSequence(max) {
     // generate a new random number between 0 and 3, and store it in a variable called randomNumber
@@ -10,7 +12,55 @@ function nextSequence(max) {
 
     // Add the new randomChosenColour generated above to the end of the gamePattern.
     gamePattern = gamePattern.push(randomChosenColor);
+    console.log("gamepattern", gamePattern);
+
+    setInterval(() => {$('#' + randomChosenColor).fadeOut().fadeIn()},500);
+
+    var queryRandomId = $('#' + randomChosenColor);
+    var audio 
+    switch (queryRandomId) {
+        case green:
+            audio = new Audio('sounds/green.mp3')
+            audio.play();
+            break;
+
+        case red:
+            audio = new Audio('sounds/red.mp3')
+            audio.play();
+            break;
+
+        case yellow:
+            audio = new Audio('sounds/yellow.mp3')
+            audio.play();
+            break;
+
+        case blue:
+            audio = new Audio('sounds/blue.mp3')
+            audio.play();
+            break;
+    
+        default:
+            audio = new Audio("sounds/wrong.mp3")
+            audio.play();
+            break;
+    }
+
+    // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    // audio.play();
 };
 
+// handle button clicked to get button id
+function handleButtonClicked() {
+    var userChosenColor = $(this).attr('id');
+    // add the contents of the var userChosenColor to the end of the userClickedPattern
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern)
+};
+
+
+// function that play sound when a a button clicked
+
+
+buttonClicked.on('click', handleButtonClicked);
 nextSequence(4)
 
