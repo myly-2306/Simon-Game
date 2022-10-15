7ac41881cf2751101e631de7dc7b45e16696e29e
My Ly <ly.thuongmy71@gmail.com>
2022-10-15 12:00:25 -0700

    final commit, game start over when user click sequence unequal to game sequence

--- game.js
+++ game.js
@@ -6,32 +6,33 @@ var started = false;
 var level = 0;
 
 
-$(document).keypress(function(){
+$(document).keypress(function() {
     if(!started) {
-        nextSequence();
         $("#level-title").text("Level" + " " + level)
+        nextSequence();
         started = true;
     }
-})
+});
 
 
 function nextSequence() {
 
     // when nextSequence() is triggered, reset userClickedPattern to an empty array
-    // userClickedPattern = [];
+    userClickedPattern = [];
 
-    var levelUp = level++;
+    levelUp = level++;
     $("#level-title").text("Level" + " " + levelUp);
 
     // generate a new random number between 0 and 3, and store it in a variable called randomNumber
     var randomNumber = Math.floor(Math.random() * 4);
     // console.log(randomNumber)
+
     // new variable called randomChosenColour and use the randomNumber above to select a random colour from the buttonColours array.
     var randomChosenColor = buttonColors[randomNumber]
 
     // Add the new randomChosenColour generated above to the end of the gamePattern.
     gamePattern.push(randomChosenColor);
-    // console.log("gamepattern", gamePattern);
+    console.log("gamepattern", " ", gamePattern);
 
     setTimeout(() => {$('#' + randomChosenColor).fadeOut().fadeIn()},500);
 
@@ -44,7 +45,8 @@ function handleButtonClicked(event) {
     event.preventDefault();
 
     var userChosenColor = $(this).attr('id');
-    // console.log("user chosen color",userChosenColor)
+    // console.log("user chosen color" + userChosenColor)
+
     // add the contents of the var userChosenColor to the end of the userClickedPattern
     userClickedPattern.push(userChosenColor);
     console.log("user click pattern" + " " + userClickedPattern)
@@ -106,26 +108,44 @@ function animatePress(currentColor) {
 
 // function to check user answer
 function checkAnswer(currentLevel) {
-    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel] ) {
         console.log("success")
         // check if the user have finshed their sequence
        if (userClickedPattern.length === gamePattern.length) {
             setTimeout(function() {
                 nextSequence()
             },1000)
-            // console.log("userPattern Length" + userClickedPattern.length)
+            console.log("userPattern Length" + " " + userClickedPattern.length)
+            console.log("gamePattern Length" + " " + gamePattern.length)
         }
     } else {
         console.log("wrong");
         audio = new Audio("sounds/wrong.mp3")
         audio.play();
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart")
+        setTimeout( () => {
+            $("body").removeClass("game-over");
+        }, 500);
+
+        startOver();
     }
     
 
     console.log("user current level" + " " +  userClickedPattern[currentLevel])
     console.log("game level" + " " + gamePattern[currentLevel])
+};
+
+
+// function to restart the game when users get the sequence wrong
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
 
 buttonClicked.on('click', handleButtonClicked);
-nextSequence()
+
+// if nextSequence is trigged outside of functions, the game will start right away and skip the entry h1 line.
+// nextSequence()
 
